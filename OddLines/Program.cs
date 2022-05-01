fe773e4eb96f1f0585dd50d1113ae3e47024bb97
Slav Tomov <stomow@outlook.com>
2022-05-01 10:52:51 +0300

    Add project files.

---
+++ OddLines/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace OddLines
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] text = File.ReadAllLines("input.txt");
+            for (int i = 0; i < text.Length; i += 2)
+            {
+                string line = text[i];
+                File.AppendAllText("output.txt", line+"\r\n");
+            }
+        }
+    }
+}
