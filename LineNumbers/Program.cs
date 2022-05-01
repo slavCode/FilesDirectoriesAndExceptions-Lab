dc450923bcc9d76cd653fda037a4fd58e4de8f18
Slav Tomov <stomow@outlook.com>
2022-05-01 11:08:43 +0300

    Add Odd Lines and Line Numbers

---
+++ LineNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LineNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] file = File.ReadAllLines("input.txt");
+            for (int i = 0; i < file.Length; i++)
+            {
+                string line = file[i];
+                int count = i + 1;
+                File.AppendAllText("output.txt", $"{count}. {line}\r\n");
+            }
+        }
+    }
+}
